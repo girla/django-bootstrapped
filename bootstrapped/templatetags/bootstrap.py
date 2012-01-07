9eb8b436b655c1ebd2a9161b6147606cb1407a40
Anatolii Kucheruk <anatolii@marpasoft.com>
2012-01-07 05:15:40 +0200

    Moved bootstrap files to separate static folder.

--- bootstrapped/templatetags/bootstrap.py
+++ bootstrapped/templatetags/bootstrap.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 register = template.Library()
 
-SCRIPT_TAG = '<script src="%sjs/bootstrap-%s.js" type="text/javascript"></script>'
+SCRIPT_TAG = '<script src="%sbootstrap/js/bootstrap-%s.js" type="text/javascript"></script>'
 
 class BootstrapJSNode(template.Node):
 
@@ -36,23 +36,23 @@ class BootstrapJSNode(template.Node):
 @register.simple_tag
 def bootstrap_custom_less(less):
     output=[
-            '<link rel="stylesheet/less" type="text/css" href="%s%s" media="all">' % (settings.STATIC_URL, less),
-            '<script src="%sjs/less-1.1.5.min.js" type="text/javascript"></script>' % settings.STATIC_URL,
+            '<link rel="stylesheet/less" type="text/css" href="%sbootstrap/%s" media="all">' % (settings.STATIC_URL, less),
+            '<script src="%sbootstrap/js/less-1.1.5.min.js" type="text/javascript"></script>' % settings.STATIC_URL,
         ]
     return '\n'.join(output)
 
 @register.simple_tag
 def bootstrap_css():
     if settings.TEMPLATE_DEBUG:
-        return '<link rel="stylesheet" type="text/css" href="%sbootstrap.css">' % settings.STATIC_URL
+        return '<link rel="stylesheet" type="text/css" href="%sbootstrap/bootstrap.css">' % settings.STATIC_URL
     else:
-        return '<link rel="stylesheet" type="text/css" href="%sbootstrap.min.css">' % settings.STATIC_URL
+        return '<link rel="stylesheet" type="text/css" href="%sbootstrap/bootstrap.min.css">' % settings.STATIC_URL
 
 @register.simple_tag
 def bootstrap_less():
     output=[
-            '<link rel="stylesheet/less" type="text/css" href="%slib/bootstrap.less">' % settings.STATIC_URL,
-            '<script src="%sless.js" type="text/javascript"></script>' % settings.STATIC_URL,
+            '<link rel="stylesheet/less" type="text/css" href="%sbootstrap/lib/bootstrap.less">' % settings.STATIC_URL,
+            '<script src="%sbootstrap/less.js" type="text/javascript"></script>' % settings.STATIC_URL,
         ]
     return '\n'.join(output)
 
